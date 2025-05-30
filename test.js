acf5852d2c71f43bb0b1da637324863b9d8b4e05
open-red-open <jackwang.m483@gmail.com>
2025-05-30 11:58:18 -0400

    !

--- test.js
+++ test.js
@@ -5,42 +5,58 @@ import { HttpsProxyAgent } from 'https-proxy-agent';
 
 
 const proxyList = [
-  "104.237.250.13:80",       // Transparent
-  "159.65.230.46:8888",      // Transparent
-  "142.93.202.130:3128",     // Transparent
-  "47.89.184.18:3128",       // Transparent
-  "23.237.210.82:80",        // Transparent (HTTPS)
-  "91.227.248.40:80",        // Transparent
-  "192.81.129.252:3132",     // Transparent (HTTPS)
-  "51.254.78.223:80",        // Transparent
-  "51.15.228.52:8080",       // Transparent
-  "152.228.154.20:80",       // Transparent
-  "98.64.128.182:3128",      // Transparent
-  "49.12.216.1:80",          // Transparent
-  "103.253.103.50:80",       // Transparent (Japan)
-  "47.91.65.23:3128",        // Transparent (Germany)
-  "94.142.139.119:31280",    // Transparent (Russia, HTTPS)
-  "91.239.7.75:80",          // Transparent (France)
-  "91.239.7.66:80",          // Transparent (France)
-  "194.183.190.10:8080",     // Transparent (Ukraine, HTTPS)
-  "38.7.197.5:999",          // Transparent (United States, HTTPS)
-  "45.204.9.197:9999",       // Transparent (South Africa)
-  "185.105.102.179:80",      // Transparent (Iran)
-  "104.248.81.109:3128",     // Transparent (United States, HTTPS)
-  "185.105.102.189:80",      // Transparent (Iran)
-  "38.250.126.201:999",      // Transparent (United States, HTTPS)
-  "41.254.63.14:8080",       // Transparent (Libya, HTTPS)
-  "185.141.213.174:8080",    // Transparent (Iran)
-  "175.116.194.101:3128",    // Transparent (Korea, HTTPS)
-  "180.89.56.240:3128",      // Transparent (China)
-  "77.75.95.14:80",          // Transparent (Lebanon)
-  "149.86.142.84:8080",      // Transparent (United States)
-  "112.126.68.169:8384",     // Transparent (China)
-  "104.129.194.46:10089",    // Transparent (United States, HTTPS)
-  "8.146.207.243:8888",      // Transparent (United States)
-  "8.140.104.98:3128",       // Transparent (United States)
-  "106.12.156.26:80",        // Transparent (China)
+    "123.141.181.49:5031",     // Korea (Republic of)
+    "84.39.112.144:3128",      // Switzerland (Zurich)
+    "46.29.162.222:80",        // Russia (Moscow)
+    "8.211.42.167:8081",       // Germany (Frankfurt am Main)
+    "141.101.123.238:80",      // United States (San Francisco)
+    "172.67.192.6:80",         // United States (San Francisco)
+    "45.131.4.232:80",         // Belize (Belize City)
+    "154.16.146.43:80",        // United States (Buffalo)
+    "172.67.176.3:80",         // United States (San Francisco)
+    "172.67.70.109:80",        // United States (San Francisco)
+    "45.131.208.93:80",        // Belize (Belize City)
+    "141.101.123.106:80",      // United States (San Francisco)
+    "66.235.200.62:80",        // United States (Jacksonville)
+    "141.101.120.46:80",       // United States (San Francisco)
+    "31.43.179.186:80",        // Kazakhstan (Almaty)
+    "45.194.53.72:80",         // United States (Phoenix)
+    "141.101.120.153:80",      // United States (San Francisco)
+    "172.67.185.157:80",       // United States (San Francisco)
+    "45.12.31.82:80",          // Belgium (Brussels)
+    "185.162.231.35:80",       // United Kingdom (London)
+    "172.67.192.9:80",         // United States (San Francisco)
+    "172.67.0.24:80",          // United States (San Francisco)
+    "185.162.231.77:80",       // United Kingdom (London)
+    "141.101.122.25:80",       // United States (San Francisco)
+    "159.112.235.41:80",       // United States (Pittsburgh)
+    "185.238.228.29:80",       // United Kingdom (Saint Albans)
+    "63.141.128.230:80",       // United States (Austin)
+    "185.162.230.50:80",       // United Kingdom (London)
+    "188.114.99.193:80",       // United States (San Francisco)
+    "45.131.6.186:80",         // Belize (Belize City)
+    "45.131.4.212:80",         // Belize (Belize City)
+    "172.67.3.140:80",         // United States (San Francisco)
+    "5.10.246.93:80",          // United Kingdom (London)
+    "45.131.208.97:80",        // Belize (Belize City)
+    "172.67.212.208:80",       // United States (San Francisco)
+    "172.67.171.211:80",       // United States (San Francisco)
+    "66.235.200.202:80",       // United States (Jacksonville)
+    "185.170.166.76:80",       // Netherlands (Willemstad)
+    "104.17.239.10:80",        // United States (San Francisco)
+    "185.162.229.93:80",       // United Kingdom (London)
+    "172.67.0.10:80",          // United States (San Francisco)
+    "185.170.166.14:80",       // Netherlands (Willemstad)
+    "141.101.120.241:80",      // United States (San Francisco)
+    "164.38.155.13:80",        // United Kingdom (Hempstead)
+    "47.238.134.126:14",       // Hong Kong (Socks4)
+    "45.12.30.237:80",         // Belgium (Brussels)
+    "154.65.39.7:80",          // United Kingdom (No City)
+    "172.64.147.72:80",        // United States (San Francisco)
+    "45.12.30.150:80",         // Belgium (Brussels)
+    "23.227.38.198:80"         // Canada
 ];
+
 
 // Test a single proxy
 async function testProxy(proxyStr) {
@@ -50,7 +66,7 @@ async function testProxy(proxyStr) {
     try {
         const response = await axios.get('https://api.ipify.org?format=json', {
             httpAgent: agent,
-            timeout: 1000,
+            timeout: 900,
         });
         console.log(`✅ Proxy ${proxyUrl} works. IP detected: ${response.data.ip}`);
         return true;
